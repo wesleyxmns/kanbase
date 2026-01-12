41dce5dbc24e2483851685a0d73d2160514bd682
Wesley Xmns <wximenes60@gmail.com>
2026-01-12 07:39:17 -0300

    delete: Arquivos de tipagens deletados pois não serão mais usados.

--- src/verify_drawer_setup.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function VerifyDrawer(): import("react/jsx-runtime").JSX.Element;
-//# sourceMappingURL=verify_drawer_setup.d.ts.map
--- src/verify_drawer_setup.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Button } from './components/ui/button';
-import { Sheet, SheetContent, SheetTrigger } from './components/ui/sheet';
-export function VerifyDrawer() {
-    return (_jsxs(Sheet, { children: [_jsx(SheetTrigger, { asChild: true, children: _jsx(Button, { children: "Open Drawer" }) }), _jsx(SheetContent, { children: _jsx("div", { children: "Drawer Content" }) })] }));
-}
