c7bdc9730ab6a3e720213c247fd628e5dbd70ee0
Wesley Xmns <wximenes60@gmail.com>
2026-01-12 08:09:47 -0300

    delete: Lib types.

--- src/lib/inject-styles.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Injeta os estilos do Kanbase automaticamente no documento
- * Isso garante que os estilos estejam disponíveis sem necessidade de import manual
- */
-export declare function injectKanbaseStyles(): void;
-//# sourceMappingURL=inject-styles.d.ts.map
